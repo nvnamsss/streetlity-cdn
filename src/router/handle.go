58e8b401cca13ff8137c985175d5dca49afd4f7b
nvnamsss <nvnam.c@gmail.com>
2020-06-30 00:35:17 +0700

    reworked the modify method, now query string will receive the encoded string

--- src/router/handle.go
+++ src/router/handle.go
@@ -15,14 +15,13 @@ import (
 	"github.com/nvnamsss/goinf/pipeline"
 )
 
-func download(w http.ResponseWriter, req *http.Request) {
+func Download(w http.ResponseWriter, req *http.Request) {
 	req.URL.RawQuery, _ = url.QueryUnescape(req.URL.RawQuery)
 	p := pipeline.NewPipeline()
 	stage := pipeline.NewStage(func() (str struct {
 		Filename string
 	}, e error) {
-		query := req.URL.Query()
-
+		query, e := url.ParseQuery(req.URL.RawQuery)
 		filenames, ok := query["f"]
 
 		if !ok {
@@ -39,6 +38,7 @@ func download(w http.ResponseWriter, req *http.Request) {
 
 	if e != nil {
 		var res Response = Response{}
+		log.Println("[Download]", "run pipeline", e.Error())
 		res.Error(e)
 		WriteJson(w, res)
 		return
@@ -51,18 +51,26 @@ func download(w http.ResponseWriter, req *http.Request) {
 	if e != nil {
 		var res Response = Response{}
 		res.Error(e)
+		log.Println("[Download]", "download", e.Error())
 		WriteJson(w, res)
 		return
 	}
 
 	w.Header().Set("Content-Disposition", "attachment; filename="+filepath.Base(filename))
 	w.Header().Set("Content-Type", req.Header.Get("Content-Type"))
-	w.Header().Set("Content-Length", req.Header.Get("Content-Length"))
+	if req.Header.Get("Content-Length") != "" {
+		w.Header().Set("Content-Length", req.Header.Get("Content-Length"))
+	}
 
-	io.Copy(w, reader)
+	_, e = io.Copy(w, reader)
+	if e != nil {
+		var res Response = Response{}
+		res.Error(e)
+		log.Println("[Download]", "copy", e.Error())
+	}
 }
 
-func upload(w http.ResponseWriter, req *http.Request) {
+func Upload(w http.ResponseWriter, req *http.Request) {
 	var res struct {
 		Response
 		Paths map[string]Response
@@ -77,7 +85,7 @@ func upload(w http.ResponseWriter, req *http.Request) {
 		Filename   []string
 		UploadType int
 	}, e error) {
-		query := req.URL.Query()
+		query, e := url.ParseQuery(req.URL.RawQuery)
 		files, ok := query["f"]
 
 		if !ok {
@@ -136,7 +144,7 @@ func upload(w http.ResponseWriter, req *http.Request) {
 	WriteJson(w, res)
 }
 
-func delete(w http.ResponseWriter, req *http.Request) {
+func Delete(w http.ResponseWriter, req *http.Request) {
 	var res Response = Response{Status: true}
 
 	req.URL.RawQuery, _ = url.QueryUnescape(req.URL.RawQuery)
@@ -166,30 +174,37 @@ func delete(w http.ResponseWriter, req *http.Request) {
 	WriteJson(w, res)
 }
 
-func modify(w http.ResponseWriter, req *http.Request) {
+func Modify(w http.ResponseWriter, req *http.Request) {
 	var res Response = Response{Status: true}
+
 	p := pipeline.NewPipeline()
 	stage := pipeline.NewStage(func() (str struct {
 		Filename   []string
+		Data       map[string]string
 		UploadType int
 	}, e error) {
-		query := req.URL.Query()
-		files, ok := query["f"]
+		req.ParseForm()
+		query, e := url.ParseQuery(req.URL.RawQuery)
+		form := req.PostForm
+		if e != nil {
+			return
+		}
 
+		files, ok := query["f"]
 		if !ok {
 			return str, errors.New("f param is missing")
 		}
 
-		t, ok := query["utype"]
-
-		if ok {
-			if v, e := strconv.Atoi(t[0]); e == nil {
-				str.UploadType = v
-				log.Println(v)
+		str.Filename = files
+		str.Data = make(map[string]string)
+		for _, f := range str.Filename {
+			if data, ok := form[f]; ok {
+				str.Data[f] = data[0]
+			} else {
+				log.Println("[Modify]", "data of", f, "is missing")
 			}
 		}
 
-		str.Filename = files
 		return
 	})
 	p.First = stage
@@ -199,32 +214,23 @@ func modify(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	req.ParseForm()
 	filenames := p.GetString("Filename")
+	datas := p.GetMapString("Data")
+
 	for _, f := range filenames {
-		file, _, e := req.FormFile(f)
-		if e != nil {
-			log.Println("[Upload]", "cannot find", f, "in the form")
-			continue
-		}
-
-		defer file.Close()
-
-		var buf bytes.Buffer
-		io.Copy(&buf, file)
-		if e := sdrive.Modify(f, buf.Bytes()); e != nil {
+		data := []byte(datas[f])
+		if e := sdrive.Modify(f, data); e != nil {
 			res.Error(e)
 		}
-		// p, e := sdrive.UploadFile(f, buf.Bytes(), sdrive.UploadType(utype))
-
-		buf.Reset()
 	}
 	WriteJson(w, res)
 }
 
 func Handle(router *mux.Router) {
 	log.Println("[Router]", "init handle")
-	router.HandleFunc("/", download).Methods("GET")
-	router.HandleFunc("/", upload).Methods("POST")
-	router.HandleFunc("/", delete).Methods("DELETE")
-	router.HandleFunc("/modify", modify).Methods("POST")
+	router.HandleFunc("/", Download).Methods("GET")
+	router.HandleFunc("/", Upload).Methods("POST")
+	router.HandleFunc("/", Delete).Methods("DELETE")
+	router.HandleFunc("/modify", Modify).Methods("POST")
 }
--- src/sdrive/modify.go
+++ src/sdrive/modify.go
@@ -8,18 +8,28 @@ import (
 )
 
 func Modify(path string, data []byte) (e error) {
-	reg := regexp.MustCompile("^[\\w\\-. ]+$")
+	reg := regexp.MustCompile("^(.+)\\/([^/]+)$")
 	if !reg.MatchString(path) {
-		log.Println("[SDrive]", "invalid filename", path)
+		log.Println("[SDrive]", "modify", "invalid filename", path)
 		return errors.New("Invalid filename")
 	}
 
+	log.Println("[SDrive]", "modify", "data", string(data))
 	path = GetPath(path)
 	if _, e = os.Stat(path); os.IsNotExist(e) {
+		log.Println("[SDrive]", "modify", "path was not exist", path)
 		return
 	}
 
-	f, _ := os.Open(path)
-	_, e = f.WriteAt(data, 0)
+	f, e := os.OpenFile(path, os.O_WRONLY, os.ModeAppend)
+	if e != nil {
+		log.Println("[SDrive]", "modify", "open", e.Error())
+	}
+	defer f.Close()
+
+	_, e = f.Write(data)
+	if e != nil {
+		log.Println("[SDrive]", "modify", "write", e.Error())
+	}
 	return
 }
